6cd3fb658d021f7aa3055bc5bf5254b3ba87db96
plun1331 <plun1331@gmail.com>
2021-01-29 21:52:13 -0800

    Edit setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="pypixel-api",
-    version="0.1.4",
+    version="0.1.5",
     description="An asynchronous wrapper for the Hypixel API.",
     long_description=README,
     long_description_content_type="text/markdown",
